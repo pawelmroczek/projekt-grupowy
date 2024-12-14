80b8ce9921f2ddf6b69f19f51b340acb97cefa01
Paweł Mroczek <pawel.mroczekpl@gmail.com>
2024-12-14 18:20:33 +0100

    feat: initial screen added

--- app/index.jsx
+++ app/index.jsx
@@ -1,22 +1,45 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-import { Link } from "expo-router";
+import {
+  Image,
+  ImageBackground,
+  ScrollView,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import backgroundImage from "../assets/backgrounds/initialBackground.png";
+import logo from "../assets/logo.png";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text className="font-pblack">Szkielet aplikacji</Text>
-      <StatusBar style="auto" />
-      <Link href="/home">SignIn</Link>
-    </View>
+    <ImageBackground source={backgroundImage} className="h-full">
+      <SafeAreaView className="h-full ">
+        <ScrollView contentContainerStyle={{ height: "90%" }}>
+          <View className="h-full justify-center items-center ">
+            <View className="items-center  p-6 rounded-xl">
+              <Image source={logo} className="w-[120px] h-[120px]"  />
+              <View className="flex-row items-center">
+                <Text className="text-[45px] font-pregular  ">Fashion </Text>
+                <Text className="text-[45px] text-primary-200  font-pblack">
+                  Buddy
+                </Text>
+              </View>
+              <Text className="text-center mt-4 text-lg font-pextralight">
+                Wykorzystaj pełen potencjał swojej szafy z Fashion Buddy!
+              </Text>
+              <TouchableOpacity className="bg-primary-100 p-2 px-8 rounded-md mt-4">
+                <Text className="text-white text-lg font-pregular ">
+                  ZALOGUJ SIĘ
+                </Text>
+              </TouchableOpacity>
+              <View className="pt-2 flex-row gap-2">
+                <Text className=" text-sm font-pregular ">lub</Text>
+                <Text className=" text-sm font-pmedium ">Zarejestruj się</Text>
+              </View>
+            </View>
+          </View>
+        </ScrollView>
+      </SafeAreaView>
+    </ImageBackground>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
