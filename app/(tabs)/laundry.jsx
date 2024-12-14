b1e662eb5bf84d5990a37bd43a757d5b6d1bc472
Paweł Mroczek <pawel.mroczekpl@gmail.com>
2024-12-14 16:04:57 +0100

    feat: add initial screens and layout components for home and authentication

---
+++ app/(tabs)/laundry.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Laundry = () => {
+  return (
+    <View>
+      <Text>Laundry</Text>
+    </View>
+  )
+}
+
+export default Laundry
--- app/index.jsx
+++ app/index.jsx
@@ -1,11 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { Link } from "expo-router";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text className="font-pblack" >Szkielet aplikacji</Text>
+      <Text className="font-pblack">Szkielet aplikacji</Text>
       <StatusBar style="auto" />
+      <Link href="/home">SignIn</Link>
     </View>
   );
 }
@@ -13,8 +15,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
