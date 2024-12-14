481bb914f33ac1ab1adbbb9a0fa63453ede0f436
Paweł Mroczek <pawel.mroczekpl@gmail.com>
2024-12-14 12:04:43 +0100

    feat: fonts added

--- app/index.jsx
+++ app/index.jsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text >Szkielet aplikacji</Text>
+      <Text className="font-pblack" >Szkielet aplikacji</Text>
       <StatusBar style="auto" />
     </View>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,37 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  
-  content: [ "./app/**/*.{js,jsx,ts,tsx}","../components/**/*.{js,jsx,ts,tsx}"],
+  content: ["./app/**/*.{js,jsx,ts,tsx}", "../components/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
-      colors:{
-        primary: "#2A9D8F",
-        primaryDark: "#264653",
-        secondary: "#E9C46A",
-        secondaryDark: "#F4A261",
-        secondaryDarker: "#E76F51",
-        textPrimary: "#333333",
-        textSecondary: "#4F4F4F",
-        textTertiary: "#828282",
-        textWhite: "#FFFFFF",
-      }
+      colors: {
+        primary: {
+          100: "#2A9D8F",
+          200: "#264653",
+        },
+        secondary: {
+          100: "#E9C46A",
+          200: "#F4A261",
+          300: "#E76F51",
+        },
+        text: {
+          primary: "#333333",
+          secondary: "#4F4F4F",
+          tertiary: "#828282",
+          white: "#FFFFFF",
+        },
+      },
+      fontFamily: {
+        pthin: ["Poppins-Thin", "sans-serif"],
+        pextralight: ["Poppins-ExtraLight", "sans-serif"],
+        plight: ["Poppins-Light", "sans-serif"],
+        pregular: ["Poppins-Regular", "sans-serif"],
+        pmedium: ["Poppins-Medium", "sans-serif"],
+        psemibold: ["Poppins-SemiBold", "sans-serif"],
+        pbold: ["Poppins-Bold", "sans-serif"],
+        pextrabold: ["Poppins-ExtraBold", "sans-serif"],
+        pblack: ["Poppins-Black", "sans-serif"],
+      },
     },
   },
   plugins: [],
-}
-
+};
