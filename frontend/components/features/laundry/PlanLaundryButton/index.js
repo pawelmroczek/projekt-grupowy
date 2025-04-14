b11b9965b774e05bd7b4cab12db6c395590fe312
Paweł Mroczek <pawel.mroczekpl@gmail.com>
2025-04-14 16:16:58 +0200

    add laundry adding

--- frontend/components/features/laundry/PlanLaundryButton/index.js
+++ frontend/components/features/laundry/PlanLaundryButton/index.js
@@ -1,9 +1,15 @@
 import { View, Text, TouchableOpacity } from "react-native";
 import React from "react";
+import { router } from "expo-router";
 
 export default function PlanLaundryButton() {
   return (
-    <TouchableOpacity className=" bg-secondary-200 p-3 rounded-xl shadow-md">
+    <TouchableOpacity
+      className=" bg-secondary-200 p-3 rounded-xl shadow-md"
+      onPress={() => {
+        router.push("/addToLaundry");
+      }}
+    >
       <Text className="text-center text-base text-white font-pmedium uppercase">
         Aktualizuj stan ubrań
       </Text>
