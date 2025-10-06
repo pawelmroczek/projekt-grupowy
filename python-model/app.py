965ee552e5b7d9e37e08773e027792101892ee52
user.email <plecmaciej2112@gmail.com>
2025-10-06 20:36:30 +0200

    adding model right classification

--- python-model/app.py
+++ python-model/app.py
@@ -1,26 +1,35 @@
-from fastapi import FastAPI, UploadFile, File
-from fastapi.responses import JSONResponse
+from fastapi import FastAPI, File, UploadFile
+from ultralytics import YOLO
 from PIL import Image
 import io
 
 app = FastAPI()
+model = YOLO("model/best.pt")
 
 @app.get("/")
 def root():
-    return {"message": "Model API działa poprawnie 🚀"}
+    return {"message": "Model API działa poprawnie :)"}
 
 @app.post("/predict")
 async def predict(image: UploadFile = File(...)):
     # odczytaj obrazek
     image_bytes = await image.read()
     img = Image.open(io.BytesIO(image_bytes))
+    
+    results = model.predict(img, conf=0.4, verbose=False)
+    detections = []
+    for result in results:
+        for box in result.boxes:
+            cls_id = int(box.cls[0])
+            conf = float(box.conf[0])
+            label = model.names[cls_id]
+            detections.append({
+                "class": label,
+                "confidence": round(conf, 3)
+            })
 
-    # tu na razie symulujemy predykcję
-    # zamiast model.predict(img) — po prostu zwracamy placeholder
-    result = {
-        "class": "example_class",
-        "confidence": 0.95,
-        "size": img.size
-    }
+    # If there was no detections
+    if not detections:
+        return {"detections": [], "message": "No symbols detected"}
 
-    return JSONResponse(content=result)
+    return {"detections": detections}
